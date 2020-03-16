798f424cd9b9cd4607f23a0ca1bb43cca8c037a4
abdurrahim <abdurrahim.ozturk93@gmail.com>
2020-03-17 00:51:02 +0100

    local branch first commit

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -3,8 +3,10 @@ package gitDay4Package;
 public class GitDay4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("local first commit master branch");
+		System.out.println("local branch first commit");
 	}
 
 }
